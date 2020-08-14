bd65ed21e6b28252f208b79be9c944109164a6b7
Ignacio Cordoba <theignaciocordoba@gmail.com>
2020-08-14 20:44:19 +0000

    Added exploration

---
+++ js/data_exploration.js
@@ -0,0 +1,72 @@
+data_exploration = [
+    {
+        title: "Navigator",
+        icon: "conqueror",
+        subtitle: "WIS (Survival)",
+        description: "Determines the route to a chosen location",
+        reference: "-",
+        bullets: [
+            "Provides all players with a +2 bonus on their respective exploration role checks on a successful Skill check."
+        ]
+    },
+    {
+        title: "Scout",
+        icon: "hood",
+        subtitle: "DEX (Stealth) and/or WIS (Perception)",
+        description: "Avoid conflict or benefit of surprise",
+        reference: "-",
+        bullets: [
+            "Moves ahead of the party, acting as a first line of defense for potential hostile encounters. Allows the party to avoid encounters on successful Skill check."
+        ]
+    },
+    {
+        title: "Lookout",
+        icon: "crow-nest",
+        subtitle: "WIS (Perception)",
+        description: "Looksout",
+        reference: "-",
+        bullets: [
+						"Prevents ambushes, spots potential clues or areas of interest when party reaches their destination. Party gets advantage on Initiative rolls on a successful Skill check."
+        ]
+    },
+		{
+        title: "Forager",
+        icon: "fishing",
+        subtitle: "INT (Nature)",
+        description: "Gathers food for the party",
+        reference: "-",
+        bullets: [
+						"Players gain 2 GP per day of travel on a successful Skill check, players lose 2 GP per day of travel on a failed Skill check."
+        ]
+    },
+		{
+        title: "Tracker",
+        icon: "deer-track",
+        subtitle: "WIS (Survival)",
+        description: "Identifies tracks to determine what creatures dwells in a certain areas",
+        reference: "-",
+        bullets: [
+						"Party learns of any remarkable creatures that may be nearby on a successful Skill check, providing insight as to what they might face."
+        ]
+    },
+		{
+        title: "Cartographer",
+        icon: "treasure-map",
+        subtitle: "DEX (Slight of Hand) and/or WIS (Nature)",
+        description: "Maps the area for the party, discovering points of interest",
+        reference: "-",
+        bullets: [
+						"Players gain advantage on their exploration role checks if traveling through an area the Cartographer has previously mapped."
+				]
+    },
+		{
+        title: "Lore Gatherer",
+        icon: "sherlock-holmes",
+        subtitle: "INT (History) and/or INT (Nature) and/or INT (Arcana)",
+        description: "Gains insight on the area that the party is exploring",
+        reference: "-",
+        bullets: [
+						"Learns lore-based information of the DM's choosing on a successful skill check."
+				]
+    },
+]
--- js/quickref.js
+++ js/quickref.js
@@ -66,6 +66,7 @@ function init() {
     fill_section(data_environment_light, "environment-light", "Environment");
     fill_section(data_environment_vision, "environment-vision", "Environment");
     fill_section(data_environment_cover, "environment-cover", "Environment");
+		fill_section(data_exploration, "basic-exploration", "Exploration");
 
     var modal = document.getElementById("modal");
     modal.onclick = hide_modal;
